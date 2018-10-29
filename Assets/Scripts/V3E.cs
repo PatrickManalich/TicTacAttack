040e4f130f52e3d707a5ea3944dd0e9a1e2e43af
PatrickManalich <patrickmanalich@gmail.com>
2018-10-28 21:28:34 -0400

    [cosmetic] Rename method parameters

--- Assets/Scripts/V3E.cs
+++ Assets/Scripts/V3E.cs
@@ -1,20 +1,17 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 /* Vector 3 Extension class that simplifies setting specific coordinate of a Vector3. */
 public static class V3E {
-
-    public static Vector3 SetX(this Vector3 vectorParam, float newX) {
-        vectorParam.x = newX;
-        return vectorParam;
+    public static Vector3 SetX(this Vector3 vector3, float newX) {
+        vector3.x = newX;
+        return vector3;
     }
-    public static Vector3 SetY(this Vector3 vectorParam, float newY) {
-        vectorParam.y = newY;
-        return vectorParam;
+    public static Vector3 SetY(this Vector3 vector3, float newY) {
+        vector3.y = newY;
+        return vector3;
     }
-    public static Vector3 SetZ(this Vector3 vectorParam, float newZ) {
-        vectorParam.z = newZ;
-        return vectorParam;
+    public static Vector3 SetZ(this Vector3 vector3, float newZ) {
+        vector3.z = newZ;
+        return vector3;
     }
 }
